652358e8998c5080b3eed79b8a717601ff95f79c
Guillaume Caugant <caugant@et.esiea.fr>
2021-03-08 21:29:14 +0100

    Stockage en memoire de l'Utilisateur cree

--- src/main/java/fr/esiea/ex4A/hello/HelloRepository.java
+++ src/main/java/fr/esiea/ex4A/hello/HelloRepository.java
@@ -2,6 +2,7 @@ package fr.esiea.ex4A.hello;
 
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 import java.util.Random;
@@ -21,6 +22,7 @@ class HelloRepository {
             "Penguine"
     );
     private final Random random = new Random();
+    final List<User> inscript = new ArrayList<User>();
 
     HelloData randomHello() {
         return new HelloData(names.get(random.nextInt(names.size())));
@@ -29,5 +31,18 @@ class HelloRepository {
     HelloData getHelloFor(String name) {
         int letterIndex = name.toLowerCase(Locale.ROOT).charAt(0) - 'a';
         return new HelloData((name + " ").repeat(1 + letterIndex).trim());
+    }
+    boolean addUser(User user)
+    {
+        if(inscript.contains(user))
+        {
+            return false;
+        }
+        else
+        {
+            inscript.add(user);
+            return true;
+        }
+
     }
 }
