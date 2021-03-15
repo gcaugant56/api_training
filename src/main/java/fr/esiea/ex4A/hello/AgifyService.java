e83d46d15d6328e70527641051d56ceec2e2f1df
Guillaume Caugant <caugant@et.esiea.fr>
2021-03-15 13:48:40 +0100

    Utilisation de l'api afigy, stockage en mémoire

--- src/main/java/fr/esiea/ex4A/hello/AgifyService.java
+++ src/main/java/fr/esiea/ex4A/hello/AgifyService.java
@@ -2,19 +2,38 @@ package fr.esiea.ex4A.hello;
 
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
+import java.util.ArrayList;
+
 @Service
 public class AgifyService {
 
     private final AgifyClient client;
 
-    public AgifyService(AgifyClient client)
+    private final HelloRepository helloRepository;
+
+    public AgifyService(AgifyClient client, HelloRepository helloRepository)
     {
         this.client = client;
+        this.helloRepository = helloRepository;
     }
 
-    public AgifyUser getAge(String name, String country)
-    {
-        return (AgifyUser) client.getAgeUser(name,country);
+    public AgifyUser getAge(String name, String country) throws IOException {
+        return (AgifyUser) client.getAgeUser(name,country).execute().body();
+    }
+
+    public ArrayList<Match> getMatches(int ageReference) throws IOException {
+        ArrayList<Match> matches = new ArrayList<Match>();
+        for(User match : helloRepository.inscript)
+        {
+            AgifyUser matchFind = getAge(match.getUserName(), match.getUserCountry());
+            if(matchFind.getAge() - ageReference < 5 && matchFind.getAge() - ageReference > -5)
+            {
+                matches.add(new Match(match.getUserName(), match.getUserTweeter()));
+            }
+        }
+        return matches;
+
     }
 
 }
--- src/main/java/fr/esiea/ex4A/hello/HelloController.java
+++ src/main/java/fr/esiea/ex4A/hello/HelloController.java
@@ -5,7 +5,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
+import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 
@@ -14,8 +14,14 @@ class HelloController {
 
     private final HelloRepository helloRepository;
 
-    HelloController(HelloRepository helloRepository) {
+    private final AgifyClient client;
+
+    private final AgifyService service;
+
+    HelloController(HelloRepository helloRepository, AgifyClient client, AgifyService service) {
         this.helloRepository = helloRepository;
+        this.client = client;
+        this.service = service;
     }
 
     @GetMapping(path = "/hello", produces = MediaType.APPLICATION_JSON_VALUE)
@@ -42,13 +48,12 @@ class HelloController {
     @GetMapping(path = "/api/matches",produces = MediaType.APPLICATION_JSON_VALUE )
     @ResponseBody
     String match(@RequestParam(name = "userName" , required = true ) String userName,
-                 @RequestParam(name = "userCountry", required = true) String userCountry) throws JsonProcessingException {
+                 @RequestParam(name = "userCountry", required = true) String userCountry) throws IOException {
+
+        client.getAgeUser(userName, userCountry);
+        AgifyUser principal = service.getAge(userName, userCountry);
+        List<Match> matches = service.getMatches(principal.getAge());
         ObjectMapper mapper = new ObjectMapper();
-        Match match = new Match("test1","test1");
-        Match match2 = new Match("test2","test2");
-        List<Match> matches = new ArrayList<>();
-        matches.add(match);
-        matches.add(match2);
         String result = mapper.writeValueAsString(matches);
         return result;
     }
