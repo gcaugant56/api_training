3b66578daa03617fdc1a2d986ee077a3296e7b08
Guillaume <caugant@et.esiea.fr>
2021-03-20 20:09:25 +0100

    test integration methode GET / POST

--- src/main/java/fr/esiea/ex4A/Controller/UserController.java
+++ src/main/java/fr/esiea/ex4A/Controller/UserController.java
@@ -28,14 +28,13 @@ class UserController {
         this.client = client;
         this.service = service;
     }
-
     @PostMapping(path = "/api/inscription",produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
-    boolean subscribe(@RequestBody Map<String,String> requestBody)
+    String subscribe(@RequestBody Map<String,String> requestBody)
     {
 
         User user = new User(requestBody.get("userEmail"),requestBody.get("userName"),requestBody.get("userTweeter"), requestBody.get("userCountry"),requestBody.get("userSex"), requestBody.get("userSexPref"));
-        return userRepository.addUser(user);
+        return "result : "+userRepository.addUser(user);
     }
 
     @GetMapping(path = "/api/matches",produces = MediaType.APPLICATION_JSON_VALUE )
@@ -43,7 +42,6 @@ class UserController {
     String match(@RequestParam(name = "userName" , required = true ) String userName,
                  @RequestParam(name = "userCountry", required = true) String userCountry) throws IOException {
 
-        client.getAgeUser(userName, userCountry);
         AgifyUser principal = service.getAge(userName, userCountry);
         List<Match> matches = service.getMatches(principal.getAge());
         ObjectMapper mapper = new ObjectMapper();
